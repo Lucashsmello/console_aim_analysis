93210fb6ec125c59d46c1ed6e71ba73562ebbb7f
Lhsmello <lucashsmello@gmail.com>
2018-12-29 21:08:47 -0200

    Updated gimxapi to 7

--- src/gimx_api.cpp
+++ src/gimx_api.cpp
@@ -1,10 +1,36 @@
 #include "gimx_api.h"
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <boost/thread.hpp>
 #include "utils/utils.h"
 #include <winsock2.h>
 #include <Ws2tcpip.h>
+
+#define MAX_AXES_REPORT 6 //max number of axes to report.
+
+typedef enum {
+	E_NETWORK_PACKET_CONTROLLER, E_NETWORK_PACKET_IN_REPORT,
+} e_network_packet_type;
+
+#pragma pack(push)  /* push current alignment to stack */
+#pragma pack(1)     /* set alignment to 1 byte boundary */
+typedef struct {
+	uint8_t index; // 1 MSB: 0 = relative axis, 1 = absolute axis, 7 LSB = index
+	uint32_t value; // network byte order
+} message_data_gimx;
+#pragma pack(pop)   /* restore original alignment from stack */
+
+#pragma pack(push)  /* push current alignment to stack */
+#pragma pack(1)     /* set alignment to 1 byte boundary */
+typedef struct {
+	uint8_t packet_type; // E_NETWORK_PACKET_IN_REPORT
+	uint8_t nbAxes; // number of elements in the following array
+	message_data_gimx axes[MAX_AXES_REPORT];
+} message_report_gimx;
+#pragma pack(pop)   /* restore original alignment from stack */
+
+#define MSG_DONTWAIT 0
 
 #ifndef __MINGW32__
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
@@ -15,7 +41,7 @@
 static char cmd[250];
 
 //static const char* SERVER_IP = "127.0.0.1";
-static const char* SERVER_IP = "192.168.0.8";
+static const char* SERVER_IP = "192.168.0.2";
 
 static int clamp(short int x, int min, int max) {
 	if (x > max) {
@@ -37,17 +63,18 @@ void pressX(bool release) {
 	int nada = system(cmd);
 }
 
-static char message[162];
-static int32_t* message_data = 0;
-static struct sockaddr_in si_other;
-static int S_CODE;
+static message_report_gimx message;
+
+#ifdef _MSC_VER
+static SOCKET S_CODE = SOCKET_ERROR;
+#else
+static int S_CODE = SOCKET_ERROR;
+#endif
 
 void initializeGIMXConnetion() {
 	WSADATA wsa;
-	memset(message, 0, 162 * sizeof(char));
-	message[0] = 0xFF;
-	message[1] = 0xA0;
-	message_data = (int32_t*) (message + 2);
+	struct sockaddr_in si_other;
+	memset(message.axes, 0, sizeof(message.axes));
 
 	//Initialise winsock
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
@@ -68,11 +95,14 @@ void initializeGIMXConnetion() {
 #else
 	InetPton(AF_INET, SERVER_IP, &si_other.sin_addr.s_addr);
 #endif
+
+	if (connect(S_CODE, (struct sockaddr*) &si_other, sizeof(si_other)) == -1) {
+		printf("connect() failed with error code : %d\n", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
 	int iTimeout = 15;
-	int iRet = setsockopt(S_CODE,
-	SOL_SOCKET,
-	SO_RCVTIMEO, (const char *) &iTimeout, sizeof(iTimeout));
-	if (iRet != 0) {
+	if (setsockopt(S_CODE, SOL_SOCKET, SO_RCVTIMEO, (const char *) &iTimeout, sizeof(iTimeout))
+			!= 0) {
 		printf("ERROR in initializeGIMXConnetion(): Could not set timeout for udp requests\n");
 	}
 
@@ -81,55 +111,56 @@ void initializeGIMXConnetion() {
 }
 
 static int sendUDPpacket(int x, int y, int shoot, unsigned int* poolTime_OUT) {
-	if (message_data == 0) {
-		printf("ERROR in sendUDPpacket(): initializeGIMXConnetion() was not called!\n");
+	if (S_CODE == SOCKET_ERROR) {
+		printf(
+				"ERROR in sendUDPpacket(): initializeGIMXConnetion() was not called or an error occurred!\n");
 		exit(EXIT_FAILURE);
 	}
-	int slen = sizeof(si_other);
 	unsigned char buf[3];
 	if (poolTime_OUT == NULL) {
-		message[0] = 0xFF;
+		message.packet_type = 0x01;
 	} else {
-		message[0] = 0xF7;
+		message.packet_type = 0xF7;
 	}
-
-	message_data[2] = x;
-	message_data[3] = y;
-	message_data[22] = shoot;
-	message_data[21] = 255;
+	message.nbAxes = 3;
+	message.axes[0].index = 2;
+	message.axes[0].value = htonl(x);
+	message.axes[1].index = 3;
+	message.axes[1].value = htonl(y);
+	message.axes[2].index = 142;
+	message.axes[2].value = htonl(shoot);
 
 //	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER_IP);
 
 //start communication
-	if (sendto(S_CODE, message, 162, 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR) {
+	if (send(S_CODE, (char*) &message,
+			sizeof(message.nbAxes) + sizeof(message.packet_type)
+					+ sizeof(message_data_gimx) * message.nbAxes,
+			MSG_DONTWAIT) == SOCKET_ERROR) {
 		printf("sendto() failed with error code : %d\n", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
-	//receive a reply and print it
-	//clear the buffer by filling null, it might have previously received data
 //	memset(buf, '\0', BUFLEN);
 //	//try to receive some data, this is a blocking call
 	if (poolTime_OUT != NULL) {
-		if (recvfrom(S_CODE, (char*) buf, 3, 0, (struct sockaddr *) &si_other,
-				&slen) == SOCKET_ERROR) {
+		if (recv(S_CODE, (char*) buf, 3, 0) == SOCKET_ERROR) {
 			printf("recvfrom() failed with error code : %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
 		}
 		poolTime_OUT[0] = (((unsigned int) buf[2]) << 8) + buf[1];
 	}
-//
+
 	return 0;
 }
 
 static const int L = 127;
-
-void setAimSpeed(int x, int y, int shoot, unsigned int* poolTime_usec_OUT) {
+void setAimSpeed(int x, int y, int shoot, unsigned int* pollTime_usec_OUT) {
 //	boost::thread T = boost::thread(boost::bind(&sendAimSpeed, x, y, shoot));
 	x = clamp(x, -L - 1, L);
 	y = clamp(y, -L - 1, L);
 #ifdef _WIN32
-	sendUDPpacket(x, y, shoot, poolTime_usec_OUT);
+	sendUDPpacket(x, y, shoot, pollTime_usec_OUT);
 #else
 	sprintf_s(cmd,
 			"gimx --event \"rel_axis_2(%d)\" --event \"rel_axis_3(%d)\" --event \"abs_axis_14(%d)\" --dst %s:51914 & > /dev/null",
